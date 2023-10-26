0b700b2557c48046847b1a789528d45faa0d2ab6
Bernard Almeida da Costa <biernard@gmail.com>
2023-10-27 00:09:37 +0200

    updates on favicon, font-family and pagination is now working

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,57 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import StorieCard from "./components/StorieCard";
 import Pagination from "react-bootstrap/Pagination";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  const [posts, setPosts] = useState(null);
-  const [error, setError] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [posts, setPosts] = useState([]); // State to store the fetched posts
+  const [error, setError] = useState(null); // State to store error messages
+  const [currentPage, setCurrentPage] = useState(1); // State to track the current page
+  const [searchQuery, setSearchQuery] = useState(""); // State to store the search query
 
   useEffect(() => {
-    // This is a side effect hook that runs when the component is mounted.
-    // It's used to fetch data when the component first loads.
+    // This useEffect hook runs when the component is mounted and whenever currentPage or searchQuery changes.
+    // It's used to fetch posts from an API.
+    fetchPosts(currentPage, searchQuery);
+  }, [currentPage, searchQuery]);
 
-    // Call the 'getData' function to get 20 current stories.
-    getData(
-      "http://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=10"
-    );
-  }, [currentPage]);
-
-  const getData = async (url) => {
-    // Define an asynchronous function 'getData' that takes a URL as a parameter for fetching data.
+  const fetchPosts = async (page, query) => {
+    // Define an asynchronous function 'fetchPosts' that takes a page number and a query as parameters.
     try {
-      const response = await axios(url); // Use Axios to make an HTTP request to the provided URL.
-      console.log(response.data.hits); // Log the data received from the API.
-      setPosts(response.data.hits); // Update the 'posts' state with the fetched data.
-      setError(null); // Reset the 'error' state to null (no error).
+      let url = `http://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=10&page=${page}`;
+      if (query) {
+        url = `http://hn.algolia.com/api/v1/search?query=${query}&tags=story&hitsPerPage=5&page=${page}`;
+      }
+      const response = await axios.get(url);
+      setPosts(response.data.hits);
+      setError(null); // Clear any previous errors
     } catch (error) {
-      setError(true); // Set 'error' state to true if there's an error.
-      setPosts(null); // Clear the 'posts' state.
+      setError("Something is wrong"); // Handle errors
+      setPosts([]); // Clear posts in case of an error
     }
   };
 
-  const searchItems = (e) => {
-    // Define a function 'searchItems' that takes an event as a parameter (usually an input change event).
-
-    // This one is for getting search results for "react" as a query.
-    // Call the 'getData' function with a dynamically generated URL based on the input value.
-    getData(
-      `http://hn.algolia.com/api/v1/search?query=${e.target.value}&tags=story&hitsPerPage=10`
-    );
-  };
-
   // Pagination function and variables
-  let active = 1;
-  let items = [];
-  for (let number = 1; number <= 5; number++) {
+  const items = [];
+  const totalPages = 5; // Change this value to the total number of pages you have.
+
+  for (let number = 1; number <= totalPages; number++) {
     items.push(
-      <Pagination.Item key={number} active={number === active}>
+      <Pagination.Item
+        key={number}
+        active={number === currentPage}
+        onClick={() => setCurrentPage(number)}
+      >
         {number}
       </Pagination.Item>
     );
   }
 
   return (
-    <div className="App d-flex flex-column justify-content-center align-items-center border border-3 border-danger px-5">
-      <div className="container d-flex flex-column gap-4 justify-content-center align-items-center border border-3 border-success p-5 m-5 bg-light rounded">
+    <div className="App d-flex flex-column justify-content-center align-items-center px-5">
+      <div className="container d-flex flex-column gap-4 justify-content-center align-items-center p-5 m-5 bg-light rounded shadow-lg">
         <img
           className="w-50 h-50 rounded shadow"
           src="src/assets/pirate.jpg"
@@ -68,18 +63,21 @@ function App() {
           type="text"
           className="form-control border-primary w-50"
           placeholder="Search for torrents..."
-          onChange={searchItems}
-        />{" "}
-        {/* Display an input field and attach an 'onChange' event handler. */}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)} // Handle input changes
+        />
+
         <div className="results-area d-flex flex-column mt-3">
           {error && <h2>Something is wrong</h2>}{" "}
-          {/* Display an error message if 'error' state is true. */}
-          {posts && posts.map((storie) => <StorieCard storie={storie} />)}
-          {/* Render 'StorieCard' components if 'posts' state is not null, passing each story as a prop. */}
+          {/* Display an error message if an error occurs */}
+          {posts.map((storie) => (
+            <StorieCard key={storie.story_id} storie={storie} /> // Render StorieCard component for each post
+          ))}
         </div>
       </div>
-      <div className="pagination-area">
-        <Pagination size="lg">{items}</Pagination>
+      <div className="pagination-area mb-3">
+        <Pagination size="lg">{items}</Pagination>{" "}
+        {/* Render pagination controls */}
       </div>
     </div>
   );
--- src/components/StorieCard.jsx
+++ src/components/StorieCard.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 
+// Define a functional component named StorieCard that takes a 'storie' prop as input
 const StorieCard = ({ storie }) => {
-  console.log(storie);
+  if (!storie) {
+    return null; // If 'storie' is null, don't render anything
+  }
+
+  console.log(storie); // Log the 'storie' object to the console
 
   return (
     <div
       className="storie-card border border-dark mb-3 rounded shadow p-2 px-4"
-      key={storie.story_id}
+      key={storie.story_id} // You can't use 'key' directly on a div; it should be used when rendering a list of elements
     >
-      <h3 className="text-primary mb-5">{storie.title}</h3>
+      <h3 className="text-primary mb-5">{storie.title}</h3>{" "}
+      {/* Display the story title */}
       <ul className="d-flex justify-content-between  gap-5 list-unstyled">
-        <li>Author: {storie.author}</li>
-        <li>Comments: {storie.num_comments}</li>
-        <li>Date: {storie.created_at}</li>
+        <li>Author: {storie.author}</li> {/* Display the author's name */}
+        <li>Comments: {storie.num_comments}</li>{" "}
+        {/* Display the number of comments */}
+        <li>Date: {storie.created_at}</li> {/* Display the creation date */}
       </ul>
     </div>
   );
